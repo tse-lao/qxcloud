bbb4412d158a0884bf25eb408f9212d2395161d6
Tse Lao <106653359+tse-lao@users.noreply.github.com>
2023-04-18 15:33:16 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,7 +26,6 @@ export default function Home() {
         <SecondaryFeatures />
         <CallToAction />
         <Testimonials />
-        <Pricing />
         <Faqs />
       </main>
       <Footer />
